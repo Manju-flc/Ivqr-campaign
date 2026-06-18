1e5dcdd9799f54eb1e1edae1312bd8c844b96312
Manju1309 <manjukrishna139@gmail.com>
2026-06-18 14:50:08 +0400

    Updated styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,36 @@
+// import Layout from "../components/Layout";
+// import LanguageToggle from "../components/LanguageToggle";
+
+// export default function Home({ t, language, setLanguage, goNext }) {
+//   return (
+//     <Layout bgImage="/images/bg-home.jpg">
+//       <div className="home-page">
+//         <div className="hero-section">
+//         <img src="/images/logo.png" className="home-logo" alt="Logo" />
+
+//         <img
+//   src={
+//     language === "ar"
+//       ? "/images/hero-text-ar.svg"
+//       : "/images/hero-text-en.svg"
+//   }
+
+// className={`hero-text-img ${language === "ar" ? "hero-text-ar" : "hero-text-en"}`}
+//   alt="Campaign text"
+// />
+// </div>
+
+//         <button className="primary-btn home-start-btn" onClick={goNext}>
+//           {t.start}
+//         </button>
+
+//         <LanguageToggle language={language} setLanguage={setLanguage} />
+//       </div>
+//     </Layout>
+//   );
+// }
+
+
 import Layout from "../components/Layout";
 import LanguageToggle from "../components/LanguageToggle";
 
@@ -5,26 +38,25 @@ export default function Home({ t, language, setLanguage, goNext }) {
   return (
     <Layout bgImage="/images/bg-home.jpg">
       <div className="home-page">
+
         <div className="hero-section">
-        <img src="/images/logo.png" className="home-logo" alt="Logo" />
+          <img src="/images/logo.png" className="home-logo" alt="Logo" />
+          <img
+            src={language === "ar" ? "/images/hero-text-ar.svg" : "/images/hero-text-en.svg"}
+            className={`hero-text-img ${language === "ar" ? "hero-text-ar" : "hero-text-en"}`}
+            alt="Campaign text"
+          />
+        </div>
 
-        <img
-  src={
-    language === "ar"
-      ? "/images/hero-text-ar.svg"
-      : "/images/hero-text-en.svg"
-  }
-//   className="hero-text-img"
-className={`hero-text-img ${language === "ar" ? "hero-text-ar" : "hero-text-en"}`}
-  alt="Campaign text"
-/>
-</div>
+        <div className="home-spacer" />
 
-        <button className="primary-btn home-start-btn" onClick={goNext}>
-          {t.start}
-        </button>
+        <div className="home-bottom">
+          <button className="primary-btn home-start-btn" onClick={goNext}>
+            {t.start}
+          </button>
+          <LanguageToggle language={language} setLanguage={setLanguage} />
+        </div>
 
-        <LanguageToggle language={language} setLanguage={setLanguage} />
       </div>
     </Layout>
   );
